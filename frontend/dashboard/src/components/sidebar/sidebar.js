67da913be72867904d305f6594b3795c34cbbbf2
stillssi <109871454+Stillssi@users.noreply.github.com>
2023-03-13 22:12:23 +0900

    sidebar btn 구현

--- frontend/dashboard/src/components/sidebar/sidebar.js
+++ frontend/dashboard/src/components/sidebar/sidebar.js
@@ -1,10 +1,37 @@
-import { useState } from "react";
 import "./sidebar.css";
+import React, { useState } from "react";
+import { Data } from "../../assets/Data/sidebarData";
+import Logo from "../../assets/img/logo.png";
+
 const Sidebar = () => {
-  const [button, setButton] = useState(false);
+  const [btnActive, setBtnActive] = useState(0);
+
+  const toggleActive = (e) => {
+    setBtnActive((prev) => {
+      return e.target.value;
+    });
+  };
+
   return (
     <div class="sidebar">
-      <div>MEDIFLEX</div>
+      <div id="logoImg">
+        <img src={Logo} alt="" />
+      </div>
+      <div className="menu">
+        {Data.map((item, idx) => {
+          return (
+            <div>
+              <button
+                value={idx}
+                className={"btn" + (idx == btnActive ? " active" : "")}
+                onClick={toggleActive}
+              >
+                {item}
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
