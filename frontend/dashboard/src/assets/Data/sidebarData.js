292a95b100fd90ae3b5f3b5fdd120fc9a99208eb
stillssi <109871454+Stillssi@users.noreply.github.com>
2023-03-14 10:12:11 +0900

    sidebar 아이콘 및 css

--- frontend/dashboard/src/assets/Data/sidebarData.js
+++ frontend/dashboard/src/assets/Data/sidebarData.js
@@ -1,9 +1,39 @@
+import {
+  UilBell,
+  UilCoffee,
+  UilCommentsAlt,
+  UilFilesLandscapes,
+  UilHome,
+  UilSetting,
+  UilUserCircle,
+} from "@iconscout/react-unicons";
 export const Data = [
-  "대시보드",
-  "회원 관리",
-  "콘텐츠 관리",
-  "알림 관리",
-  "댓글 관리",
-  "고객센터",
-  "기본설정",
+  {
+    icon: UilHome,
+    heading: "대시보드",
+  },
+  {
+    icon: UilUserCircle,
+    heading: "회원 관리",
+  },
+  {
+    icon: UilFilesLandscapes,
+    heading: "콘텐츠 관리",
+  },
+  {
+    icon: UilBell,
+    heading: "알림 관리",
+  },
+  {
+    icon: UilCommentsAlt,
+    heading: "댓글 관리",
+  },
+  {
+    icon: UilCoffee,
+    heading: "고객센터",
+  },
+  {
+    icon: UilSetting,
+    heading: "기본설정",
+  },
 ];
--- frontend/dashboard/src/components/sidebar/sidebar.js
+++ frontend/dashboard/src/components/sidebar/sidebar.js
@@ -2,31 +2,31 @@ import "./sidebar.css";
 import React, { useState } from "react";
 import { Data } from "../../assets/Data/sidebarData";
 import Logo from "../../assets/img/logo.png";
-
 const Sidebar = () => {
   const [btnActive, setBtnActive] = useState(0);
 
   const toggleActive = (e) => {
     setBtnActive((prev) => {
-      return e.target.value;
+      return parseInt(e.target.value);
     });
   };
 
   return (
-    <div class="sidebar">
+    <div className="sidebar">
       <div id="logoImg">
         <img src={Logo} alt="" />
       </div>
       <div className="menu">
         {Data.map((item, idx) => {
           return (
-            <div>
+            <div className="sidebtn-container" key={idx}>
               <button
                 value={idx}
-                className={"btn" + (idx == btnActive ? " active" : "")}
+                className={"btn" + (idx === btnActive ? " active" : "")}
                 onClick={toggleActive}
               >
-                {item}
+                <item.icon className="icon" />
+                {item.heading}
               </button>
             </div>
           );
