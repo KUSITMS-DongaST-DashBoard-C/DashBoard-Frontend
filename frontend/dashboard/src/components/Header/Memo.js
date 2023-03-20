c9b091ac59d9f34b670128cd97188ea0c8257a11
anjiniii <anjin720jin@naver.com>
2023-03-20 16:45:17 +0900

    [Feat] Memo button click animation

--- frontend/dashboard/src/components/Header/Memo.js
+++ frontend/dashboard/src/components/Header/Memo.js
@@ -1,13 +1,32 @@
 import { UilCommentAlt } from "@iconscout/react-unicons";
+import { useState } from "react";
 import "./Memo.css";
 
 const Memo = () => {
+  const [isOpenMemo, setIsOpenMemo] = useState(false);
+
   return (
     <>
-      <button>
-        <UilCommentAlt className="comment-icon" />
-      </button>
-      {/* <div id="circle"></div> */}
+      <div className="memo">
+        <button
+          onClick={() => {
+            setIsOpenMemo(!isOpenMemo);
+          }}
+          className={
+            isOpenMemo
+              ? "memo-button memo-button-opened"
+              : "memo-button memo-button-closed"
+          }
+        >
+          <UilCommentAlt />
+        </button>
+        {/* <div id="circle"></div> */}
+        {isOpenMemo && (
+          <div className="memo-container">
+            <div className="memo-item">AA</div>
+          </div>
+        )}
+      </div>
     </>
   );
 };
