a4853fdbc7613d88205e377645a56d2aa7260e44
anjiniii <anjin720jin@naver.com>
2023-03-20 13:08:29 +0900

    [Feat] LineCharts chart UI

--- frontend/dashboard/src/components/chart/LineCharts.js
+++ frontend/dashboard/src/components/chart/LineCharts.js
@@ -1,4 +1,99 @@
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 import "./LineCharts.css";
+
+const data = [
+  {
+    name: "10월",
+    uv: 15,
+    pv: 24,
+    amt: 24,
+  },
+  {
+    name: "11월",
+    uv: 30,
+    pv: 13,
+    amt: 22,
+  },
+  {
+    name: "12월",
+    uv: 20,
+    pv: 98,
+    amt: 22,
+  },
+  {
+    name: "1월",
+    uv: 27,
+    pv: 39,
+    amt: 20,
+  },
+  {
+    name: "2월",
+    uv: 18,
+    pv: 48,
+    amt: 21,
+  },
+  {
+    name: "3월",
+    uv: 23,
+    pv: 38,
+    amt: 25,
+  },
+];
+
+function Chart() {
+  return (
+    <ResponsiveContainer width="100%" height={268}>
+      <AreaChart
+        className="area-chart"
+        data={data}
+        margin={{
+          top: 0,
+          right: 0,
+          left: 0,
+          bottom: 0,
+        }}
+      >
+        <CartesianGrid vertical={false} />
+        <XAxis dataKey="name" axisLine={false} fontSize={13} tickLine={false} />
+        <YAxis
+          type="number"
+          axisLine={false}
+          fontSize={13}
+          tickLine={false}
+          width={30}
+        />
+        <Tooltip />
+        <Area
+          type="linear"
+          dataKey="uv"
+          stroke="#FB6358"
+          strokeWidth={2}
+          fill="#FB6358"
+          fillOpacity={0.2}
+          isAnimationActive={false}
+        />
+        <Area
+          type="linear"
+          dataKey="pv"
+          stroke="#FB6358"
+          strokeWidth={2}
+          fill="#FB6358"
+          fillOpacity={0.1}
+          isAnimationActive={false}
+        />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
+}
+
 const LineCharts = () => {
   return (
     <div className="linecharts-container">
@@ -8,7 +103,9 @@ const LineCharts = () => {
           <span className="WAU">WAU</span>
           <span className="MAU">MAU</span>
         </div>
-        <div className="linecharts-chart">A</div>
+        <div className="linecharts-chart">
+          <Chart />
+        </div>
       </div>
       <div className="linecharts-stats">
         <span>03-16 기준</span>
