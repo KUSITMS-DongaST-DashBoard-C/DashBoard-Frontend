ce0e08936440afd482541323f02f7bca6c29bb93
anjiniii <anjin720jin@naver.com>
2023-03-20 12:47:51 +0900

    [Feat] LineCharts structure

--- frontend/dashboard/src/components/chart/LineCharts.js
+++ frontend/dashboard/src/components/chart/LineCharts.js
@@ -1,6 +1,20 @@
 import "./LineCharts.css";
 const LineCharts = () => {
-  return <div className="linechart-container">선 그래프</div>;
+  return (
+    <div className="linecharts-container">
+      <div className="linecharts-charts">
+        <div className="linecharts-button">
+          <span className="DAU">DAU</span>
+          <span className="WAU">WAU</span>
+          <span className="MAU">MAU</span>
+        </div>
+        <div className="linecharts-chart">A</div>
+      </div>
+      <div className="linecharts-stats">
+        <span>03-16 기준</span>
+      </div>
+    </div>
+  );
 };
 
 export default LineCharts;
