6c1b3b1d4e439d6851d048664aa0278c431bb7b1
anjiniii <anjin720jin@naver.com>
2023-03-20 22:40:39 +0900

    [Feat] fetch mau/wau/dau data

--- frontend/dashboard/src/components/chart/line-charts/LineCharts.js
+++ frontend/dashboard/src/components/chart/line-charts/LineCharts.js
@@ -1,3 +1,5 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import {
   AreaChart,
   Area,
@@ -10,51 +12,57 @@ import {
 import "./LineCharts.css";
 import UserStats from "./UserStats";
 
-const data = [
-  {
-    name: "10월",
-    uv: 15,
-    pv: 24,
-    amt: 24,
-  },
-  {
-    name: "11월",
-    uv: 30,
-    pv: 13,
-    amt: 22,
-  },
-  {
-    name: "12월",
-    uv: 20,
-    pv: 98,
-    amt: 22,
-  },
-  {
-    name: "1월",
-    uv: 27,
-    pv: 39,
-    amt: 20,
-  },
-  {
-    name: "2월",
-    uv: 18,
-    pv: 48,
-    amt: 21,
-  },
-  {
-    name: "3월",
-    uv: 23,
-    pv: 38,
-    amt: 25,
-  },
+const dauData = [
+  { daysBefore: 0, dau: 10, signupNum: 0 },
+  { daysBefore: 1, dau: 17, signupNum: 3 },
+  { daysBefore: 2, dau: 18, signupNum: 3 },
+  { daysBefore: 3, dau: 17, signupNum: 7 },
+  { daysBefore: 4, dau: 14, signupNum: 6 },
+  { daysBefore: 5, dau: 13, signupNum: 4 },
+  { daysBefore: 6, dau: 16, signupNum: 4 },
 ];
 
-const Chart = () => {
-  return (
+const Chart = ({ selectedChart }) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios
+        .get("http://43.201.80.154:80/chart/".concat(selectedChart))
+        .then((res) => res.data);
+
+      setData(response.data);
+      setIsLoading(false);
+    };
+    getData();
+  });
+
+  const chartData = data.reverse().map(function (el) {
+    let obj = {};
+    if (selectedChart === "dau") {
+      obj["index"] = el.daysBefore;
+      obj["value"] = el.dau;
+    } else if (selectedChart === "wau") {
+      obj["index"] = el.weeksNum;
+      obj["value"] = el.wau;
+    } else if (selectedChart === "mau") {
+      obj["index"] = el.monthsBefore;
+      obj["value"] = el.mau;
+    }
+
+    obj["signupNum"] = el.signupNum;
+
+    return obj;
+  });
+
+  return isLoading ? (
+    <span>Loading...</span>
+  ) : (
     <ResponsiveContainer className="linecharts-chart" width="100%" height={240}>
       <AreaChart
         className="area-chart"
-        data={data}
+        data={chartData}
         margin={{
           top: 0,
           right: 0,
@@ -63,7 +71,12 @@ const Chart = () => {
         }}
       >
         <CartesianGrid vertical={false} />
-        <XAxis dataKey="name" axisLine={false} fontSize={13} tickLine={false} />
+        <XAxis
+          dataKey="index"
+          axisLine={false}
+          fontSize={13}
+          tickLine={false}
+        />
         <YAxis
           type="number"
           axisLine={false}
@@ -74,20 +87,20 @@ const Chart = () => {
         <Tooltip />
         <Area
           type="linear"
-          dataKey="uv"
-          stroke="#FB6358"
-          strokeWidth={2}
-          fill="#FB6358"
-          fillOpacity={0.2}
-          isAnimationActive={false}
-        />
-        <Area
-          type="linear"
-          dataKey="pv"
+          dataKey="value"
           stroke="#FB6358"
           strokeWidth={2}
           fill="#FB6358"
           fillOpacity={0.1}
+          isAnimationActive={false}
+        />
+        <Area
+          type="linear"
+          dataKey="signupNum"
+          stroke="#FB6358"
+          strokeWidth={2}
+          fill="#FB6358"
+          fillOpacity={0.2}
           isAnimationActive={false}
         />
       </AreaChart>
@@ -95,22 +108,59 @@ const Chart = () => {
   );
 };
 
-const LineChartsButton = () => {
+const LineChartsButton = (props) => {
   return (
-    <div className="linecharts-button">
-      <span className="DAU">DAU</span>
-      <span className="WAU">WAU</span>
-      <span className="MAU">MAU</span>
+    <div className="linecharts-button-container">
+      <button
+        onClick={() => {
+          props.setSelectedChart("dau");
+        }}
+        className={
+          props.selectedChart === "dau"
+            ? "selectedChart linecharts-button"
+            : "linecharts-button"
+        }
+      >
+        DAU
+      </button>
+      <button
+        onClick={() => {
+          props.setSelectedChart("wau");
+        }}
+        className={
+          props.selectedChart === "wau"
+            ? "selectedChart linecharts-button"
+            : "linecharts-button"
+        }
+      >
+        WAU
+      </button>
+      <button
+        onClick={() => {
+          props.setSelectedChart("mau");
+        }}
+        className={
+          props.selectedChart === "mau"
+            ? "selectedChart linecharts-button"
+            : "linecharts-button"
+        }
+      >
+        MAU
+      </button>
     </div>
   );
 };
 
 const LineCharts = () => {
+  const [selectedChart, setSelectedChart] = useState("dau");
   return (
     <div className="linecharts-container">
       <div className="linecharts-charts">
-        <LineChartsButton />
-        <Chart />
+        <LineChartsButton
+          selectedChart={selectedChart}
+          setSelectedChart={setSelectedChart}
+        />
+        <Chart selectedChart={selectedChart} />
       </div>
       <UserStats />
     </div>
