d385ee3e80b996fb66aabeb234cdc904dd7993e9
stillssi <109871454+Stillssi@users.noreply.github.com>
2023-03-21 22:43:46 +0900

    [Feat] PieChart tooltip Customizing

--- frontend/dashboard/src/components/chart/piechart.js
+++ frontend/dashboard/src/components/chart/piechart.js
@@ -11,14 +11,67 @@ import {
   Legend,
 } from "recharts";
 
-// const data = [
-//   { name: "정형외과", value: 400 },
-//   { name: "내과", value: 300 },
-//   { name: "내분비내과", value: 300 },
-//   { name: "신경과", value: 200 },
-//   { name: "그 외", value: 100 },
-// ];
+const MajorCustomTooltip = ({ active, payload, label }) => {
+  if (active && payload && payload.length) {
+    console.log(payload[0].payload, label);
+    return (
+      <div className="custom-tooltip">
+        {/* <p className="label">{`${payload[0].name} : ${payload[0].value}`}</p> */}
+        <div>
+          {payload.map((pld) => (
+            <div
+              style={{
+                display: "inline-block",
+                padding: "1rem",
+                backgroundColor: "white",
+                borderRadius: "1rem",
+                boxShadow: "0px 0px 8px 4px rgba(173, 181, 189, 0.15)",
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  alignContent: "center",
+                  justifyContent: "center",
+                  color: "#343A40",
+                  fontWeight: "500",
+                  marginBottom: "0.5rem",
+                  fontSize: "14px",
+                }}
+              >{`${pld.name}`}</div>
+              <div
+                style={{
+                  display: "flex",
+                }}
+              >
+                <div
+                  style={{
+                    width: "100%",
+                    color: "#868E96",
+                    marginRight: "1rem",
+                    fontWeight: "500",
+                    fontSize: "13px",
+                  }}
+                >
+                  총 회원 수
+                </div>
+                <div
+                  style={{
+                    color: "#FB6358",
+                    fontWeight: "500",
+                    fontSize: "13px",
+                  }}
+                >{`${pld.value} `}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 
+  return null;
+};
 const COLORS = ["#FB6358", "#FC9481", "#FDB19A", "#FED1BC", "#FEEADD"];
 
 const renderColorfulLegendText = (value) => {
@@ -68,7 +121,10 @@ export default function PieC() {
               />
             ))}
           </Pie>
-          <Tooltip />
+          <Tooltip
+            content={<MajorCustomTooltip />}
+            cursor={{ fill: "transparent" }}
+          />
           <Legend
             verticalAlign="top"
             align="right"
