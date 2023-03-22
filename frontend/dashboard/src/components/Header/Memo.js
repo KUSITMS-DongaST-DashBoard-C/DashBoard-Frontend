b41bf387e00a8a335303f81cdc4892c6e7973f52
anjiniii <anjin720jin@naver.com>
2023-03-23 00:51:25 +0900

    [Feat] memoItem UI

--- frontend/dashboard/src/components/Header/Memo.js
+++ frontend/dashboard/src/components/Header/Memo.js
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MdOutlineModeComment } from "react-icons/md";
+import { BsPlusCircleFill } from "react-icons/bs";
+import { AiOutlineSend } from "react-icons/ai";
 import MemoItem from "./MemoItem";
 import "./Memo.css";
 
 const Memo = () => {
   const [isOpenMemo, setIsOpenMemo] = useState(false);
+  const [isOpenNewMemo, setIsOpenNewMemo] = useState(false);
+  const [newMemoText, setNewMemoText] = useState("");
 
   return (
     <>
@@ -24,7 +28,34 @@ const Memo = () => {
         {/* <div id="circle"></div> */}
         {isOpenMemo && (
           <div className="memo-container">
-            <div className="memo-item-header">new Memo</div>
+            <div className={"memo-item-header"}>
+              <button
+                onClick={() => {
+                  setIsOpenNewMemo(!isOpenNewMemo);
+                }}
+              >
+                <BsPlusCircleFill
+                  size={24}
+                  style={{
+                    color: isOpenNewMemo ? "black" : "rgba(134, 142, 150, 1)",
+                  }}
+                />
+              </button>
+            </div>
+            {isOpenNewMemo && (
+              <div className="new-memo">
+                <span className="new-memo-name">관리자1</span>
+                <textarea
+                  type="text"
+                  placeholder="새 메모를 작성하세요."
+                  className="new-memo-text"
+                  onChange={(event) => setNewMemoText(event.target.value)}
+                />
+                <button className="new-memo-send">
+                  <AiOutlineSend size={20} />
+                </button>
+              </div>
+            )}
             <div className="memo-items">
               <MemoItem />
               <MemoItem />
--- frontend/dashboard/src/components/Header/MemoItem.js
+++ frontend/dashboard/src/components/Header/MemoItem.js
@@ -3,9 +3,26 @@ import profileImg2 from "../../assets/img/profile2.svg";
 import profileImg3 from "../../assets/img/profile3.svg";
 import { RxDotsVertical } from "react-icons/rx";
 import { BiChevronUp, BiChevronDown } from "react-icons/bi";
+import { GoPencil } from "react-icons/go";
+import { AiOutlineSend } from "react-icons/ai";
 import "./MemoItem.css";
+import { useState } from "react";
+
+const CommentItem = ({ img, name, content }) => {
+  return (
+    <div className="comment-item">
+      <img className="comment-profile-img" src={img} alt="" />
+      <span className="comment-name">{name}</span>
+      <span className="comment-content">{content}</span>
+    </div>
+  );
+};
 
 const MemoItem = () => {
+  const [isCommentOpened, setIsCommentOpened] = useState(false);
+  const [isCommentWriteOpened, setIsCommentWriteOpened] = useState(false);
+  const [newCommentText, setNewCommentText] = useState("");
+
   return (
     <div className="memo-item">
       <img className="memo-profile-img" src={profileImg1} alt="" />
@@ -17,19 +34,76 @@ const MemoItem = () => {
           </div>
           <div className="memo-setting">
             <button>
-              <RxDotsVertical size={24} />
+              <RxDotsVertical size={20} />
             </button>
           </div>
         </div>
         <p className="memo-content">
           큐시즘 관리자님, 지역별 트래픽 이슈 건 금일 중으로 확인 부탁드립니다.
         </p>
-        <div className="coment-header">
-          <img className="coment-profile-img" src={profileImg2} alt="" />
-          <img className="coment-profile-img" src={profileImg3} alt="" />
-          <span className="coment-num">댓글 2</span>
-          <BiChevronDown size={24} />
+        <div className="comment-header">
+          <button
+            onClick={() => {
+              setIsCommentOpened(!isCommentOpened);
+            }}
+            className="comment-btn-header"
+          >
+            <img
+              className="comment-header-profile-img"
+              src={profileImg2}
+              alt=""
+            />
+            <img
+              className="comment-header-profile-img"
+              src={profileImg3}
+              alt=""
+            />
+            <span className="comment-num">댓글 2</span>
+            {isCommentOpened ? (
+              <BiChevronUp size={24} />
+            ) : (
+              <BiChevronDown size={24} />
+            )}
+          </button>
+          <button
+            onClick={() => {
+              // setIsCommentOpened(true);
+              setIsCommentWriteOpened(!isCommentWriteOpened);
+            }}
+          >
+            <GoPencil
+              size={20}
+              color={isCommentWriteOpened ? "black" : "#868E96"}
+            />
+          </button>
         </div>
+        {isCommentOpened && (
+          <>
+            <CommentItem
+              img={profileImg2}
+              name="관리자2"
+              content="네 확인했습니다."
+            />
+            <CommentItem
+              img={profileImg3}
+              name="관리자3"
+              content="네 확인했습니다. 혹시 오후 회의 때 참여 가능하신가요?"
+            />
+          </>
+        )}
+        {isCommentWriteOpened && (
+          <div className="new-comment">
+            <textarea
+              type="text"
+              placeholder="새 댓글을 작성하세요."
+              className="new-comment-text"
+              onChange={(event) => setNewCommentText(event.target.value)}
+            />
+            <button className="new-comment-send">
+              <AiOutlineSend size={20} />
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
