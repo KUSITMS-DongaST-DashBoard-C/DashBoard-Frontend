29c5b5f7552fa01696573dd0336af1bdfa2ce632
stillssi <109871454+Stillssi@users.noreply.github.com>
2023-03-23 15:18:49 +0900

    [Feat] 카테고리 별 필터 구현

--- frontend/dashboard/src/api/AnalyzeVideoData.js
+++ frontend/dashboard/src/api/AnalyzeVideoData.js
@@ -2,7 +2,7 @@ import axios from "axios";
 export async function AnalyzeVideoData(category, sortby, startDate, endDate) {
   const response = await axios
     .get(
-      `http://43.201.80.154:80/${category}/${sortby}?endDate=${endDate}&startDate=${startDate}`
+      `http://43.201.80.154:80/${category}/detail/${sortby}?endDate=${endDate}&startDate=${startDate}`
     )
     .then((res) => res.data);
 
